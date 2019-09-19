282646ce9f41d2ebb13da5db9a09c09c4e281bfe
tjeldflaat <43675119+Yorshka9@users.noreply.github.com>
2019-09-19 23:47:27 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,12 +4,14 @@ const buttonReset = document.querySelector(".btn_reset");
 const buttonBacktoMenu = document.querySelector(".btn_back_to_menu");
 const overlayTitle = document.querySelector("h2");
 const levelSelect = document.querySelector(".choose-level");
+let card = document.getElementsByClassName(".card");
 let reset = false;
 
 // sound variables
 let chooseLevel = new Audio('sounds/choose-level.mp3');
 let levelSelected = new Audio('sounds/level-selected.mp3');
 let isPlaying = false;
+
 
 
 //function for allowing the user to pause the background music when the a element is clicked
@@ -27,35 +29,26 @@ function togglePlay(){
     overlay.style.display = "none";
     chooseLevel.play();
 
+    
+
  });
 
     // when player selects a level, play obnoxious sound
     levelSelect.addEventListener("click", () => {
     levelSelected.play();
-    chooseLevel.pause();
   
    });
 
-   // function for 
-   function play_single_sound(target) {
-   document.getElementById(target).play();
-}
+   /*
+    // when player selects a level, play obnoxious sound
+    levelSelect.addEventListener("mouseover", () => {
+        levelSelected.play();
+        chooseLevel.togglePlay();
+      
+       });
+    */
 
-function newPage(url) {
-    location.href = url;
-}
-
-function playThenRedirectTo(audioTarget, url) {
-    var time = 0;
-
-    play_single_sound(audioTarget);
-
-    setInterval(function () {
-        var end = document.getElementById(audioTarget).played.end(0);
-        if (end > time) {
-            time = end;
-        } else {
-            newPage(url);
-        }
-    }, 250);
-}
+       // function for la
+       function delay (URL) {
+        setTimeout( function() { window.location = URL }, 1500 );
+    }
